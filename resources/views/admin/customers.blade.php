bf9461d39a9f6ffb4585ed11e467e9d3e4382c8f
adiarhmn <adiauliarahmn@gmail.com>
2025-08-04 06:10:14 +0800

    create admin layout

---
+++ resources/views/admin/customers.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.admin-layout')
+@section('content')
+    <div class="content-page">
+        <div class="content">
+            <div class="container-fluid">
+                <div class="py-3 d-flex align-items-sm-center flex-sm-row flex-column">
+                    <div class="flex-grow-1">
+                        <h4 class="fs-18 fw-semibold m-0">Customers</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('layouts.admin-layout');
+    return view('admin.dashboard');
+});
+
+/*
+    AUTHENTICATION ROUTES ============================================>
+*/
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+
+/*
+    ADMIN ROUTES ============================================>
+*/
+Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'admin']], function () {
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+
+    Route::prefix('customers')
+        ->controller(
+            UserController::class
+        )->group(function () {
+            Route::get('/', 'showCustomers');
+        });
 });
